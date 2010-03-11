b434854eec79c839c6003518aec4b65c8766d10e
danielemar86 <danielemar86@gmail.com>
2010-03-11 10:50:03 +0100

    Testing hg

---
+++ Darpooling/Darpooling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Darpooling
+{
+    class User
+    {
+        enum Sex {f, m};
+        int userId;
+        String username;
+        String name;
+        Sex sex;
+        DateTime birthDate;
+        String email;
+        Boolean smoker;
+        DateTime signupDate;
+        String whereabouts;
+    }
+
+    // Vediamo se funge
+
+    class Path
+    {
+        Int32 id;
+        DateTime departureDateTime;
+        DateTime arrivalDateTime;
+        String departureName;
+        String arrivalName;   
+        Boolean smoke;
+        Boolean music;
+        Double cost;
+        User owner;
+        int freeSits;
+        String notes;
+        Boolean modifiable;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
