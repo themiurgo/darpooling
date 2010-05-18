c7bb6d6ab920128dae962ab2c03693e524908279
anto87 <anto87@localhost>
2010-05-18 12:56:06 +0200

    Added ServiceNode

---
+++ Darpooling/Darpooling/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Darpooling
+{
+    public class User
+    {
+        enum Sex {f, m};
+        int userId;
+        String username;
+        String name;
+        Sex sex;
+        DateTime birthDate;
+        String email;
+        Boolean smoker;
+        DateTime signupDate;
+        String whereabouts;
+
+        public User(String name) 
+        {
+            this.name = name;
+        }
+
+        public String Name 
+        {
+            get 
+            {
+                return name;
+            }
+         }
+    }
+
+    public class Trip
+    {
+        Int32 id;
+        DateTime departureDateTime;
+        DateTime arrivalDateTime;
+        String departureName;
+        String arrivalName;   
+        Boolean smoke;
+        Boolean music;
+        Double cost;
+        User owner;
+        int freeSits;
+        String notes;
+        Boolean modifiable;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User antonio = new User("Antonio");
+            Console.WriteLine("Nome utente : {0}", antonio.Name);
+            Console.ReadLine();
+        }
+    }
+};
---
+++ Darpooling/ServiceNode/ServiceNode/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Math;
+
+namespace ServiceNode
+{
+    class Location
+    {
+        double latitude;
+        double longitude;
+        double r = 6378;
+
+        public Location(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+
+        public double distance(Location other)
+        {
+            double a1 = this.latitude;
+            double b1 = this.longitude;
+            double a2 = other.latitude;
+            double b2 = other.longitude;
+            return Math.Acos(Math.Cos(a1)*Math.Cos(b1)*Math.Cos(a2)*Math.Cos(b2) + Math.Cos(a1)*Math.Sin(b1)*Math.Cos(a2)*Math.Sin(b2) + Math.Sin(a1)*Math.Sin(a2)) * r;
+        }
+    }
+
+    class Node
+    {
+        string address;
+        string location;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Location p1 = new Location(Math.PI/2, Math.PI/2);
+            Location p2 = new Location(Math.PI/2, Math.PI);
+            Console.WriteLine(p1.distance(p2));
+        }
+    }
+}
