0d7dbcfb5e512e74a0361a8ab70e0c423b65abb0
anto87 <anto87@localhost>
2010-06-24 18:24:19 +0200

    Riordino

--- Darpooling/Darpooling/Program.cs
+++ Darpooling/Darpooling/Program.cs
@@ -5,6 +5,34 @@ using System.Text;
 
 namespace Darpooling
 {
+    class Location
+    {
+        double latitude;
+        double longitude;
+        double r = 6378;
+
+        public Location(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            Console.WriteLine("New Location {0} {1}", latitude, longitude);
+        }
+
+
+        public double distance(Location other)
+        {
+            double a1 = this.latitude;
+            double b1 = this.longitude;
+            double a2 = other.latitude;
+            double b2 = other.longitude;
+
+
+            double a = Math.Acos(Math.Cos(a1) * Math.Cos(b1) * Math.Cos(a2) * Math.Cos(b2) + Math.Cos(a1) * Math.Sin(b1) * Math.Cos(a2) * Math.Sin(b2) + Math.Sin(a1) * Math.Sin(a2)) * r;
+            Console.WriteLine("{0}", a);
+            return a;
+        }
+    }
+
     public class User
     {
         enum Sex {f, m};
@@ -58,6 +86,13 @@ namespace Darpooling
             Console.WriteLine("Nome utente : {0}", antonio.Name);
             Console.WriteLine("Nome utente : {0}", daniele.Name);
             Console.ReadLine();
+
+            Location p1 = new Location(Math.PI / 2, Math.PI / 2);
+            Location p2 = new Location(Math.PI / 2, Math.PI);
+            Console.WriteLine(p1.distance(p2));
+            Console.ReadLine();
         }
     }
+
+
 };
