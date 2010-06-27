ee5675d41ae9ce24530d3dc8ff80b577f0291395
anto87 <anto87@localhost>
2010-06-27 20:15:01 +0200

    Adding LoggedState

--- Client/State.cs
+++ Client/State.cs
@@ -6,56 +6,114 @@ using Communication;
 
 namespace Client
 {
-    public interface State
+    public interface IState
     {
-        bool Join(string address);
-        bool Unjoin();
-        bool NewTrip(Communication.Trip trip);
-        bool SearchTrip();
+        bool Join(ClientCore context, string address);
+        bool RegisterUser(string username, string pw_hash);
+        bool LoginUser(string username, string pw_hash);
+        bool Unjoin(ClientCore context);
+        bool InsertTrip(ClientCore context, Communication.Trip trip);
+        bool SearchTrip(ClientCore context);
     }
 
-    public class DisconnectedState : State
+    public class DisconnectedState : IState
     {
-        public bool Join(string address)
+        public bool Join(ClientCore context, string address)
         {
             // Connect operations
+            context.State = new ConnectedState();
             return true;
         }
 
-        public bool Unjoin()
+        public bool RegisterUser(string username, string pw_hash)
         {
             return false;
         }
 
-        public bool NewTrip(Communication.Trip trip)
+        public bool LoginUser(string username, string pw_hash)
         {
             return false;
         }
 
-        public bool SearchTrip()
+        public bool Unjoin(ClientCore context)
+        {
+            return false;
+        }
+
+        public bool InsertTrip(ClientCore context, Communication.Trip trip)
+        {
+            return false;
+        }
+
+        public bool SearchTrip(ClientCore context)
         {
             return false;
         }
     }
 
-    public class ConnectedState : State
+    public class ConnectedState : IState
     {
-        public bool Join(string address)
+        public bool Join(ClientCore context, string address)
         {
             return false;
         }
 
-        public bool Unjoin()
+        public bool Unjoin(ClientCore context)
         {
+            context.State = new DisconnectedState();
             return true;
-       }
+        }
 
-        public bool NewTrip(Communication.Trip Trip)
+        public bool RegisterUser(string username, string pw_hash)
+        {
+            return false;
+        }
+
+        public bool LoginUser(string username, string pw_hash)
+        {
+            return false;
+        }
+
+        public bool InsertTrip(ClientCore context, Communication.Trip Trip)
         {
             return true;
         }
 
-        public bool SearchTrip()
+        public bool SearchTrip(ClientCore context)
+        {
+            return true;
+        }
+    }
+
+    public class LoggedState : IState
+    {
+        public bool Join(ClientCore context, string address)
+        {
+            return false;
+        }
+
+        public bool Unjoin(ClientCore context)
+        {
+            context.State = new DisconnectedState();
+            return true;
+        }
+
+        public bool RegisterUser(string username, string pw_hash)
+        {
+            return false;
+        }
+
+        public bool LoginUser(string username, string pw_hash)
+        {
+            return false;
+        }
+
+        public bool InsertTrip(ClientCore context, Communication.Trip Trip)
+        {
+            return true;
+        }
+
+        public bool SearchTrip(ClientCore context)
         {
             return true;
         }
