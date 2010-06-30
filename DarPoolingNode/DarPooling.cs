3fd83134494fb4face3e06873fc6633c7113588d
danielemar86 <danielemar86@gmail.com>
2010-06-30 21:58:22 +0200

    Starting two service nodes with MEX enabled

--- DarPoolingNode/DarPooling.cs
+++ DarPoolingNode/DarPooling.cs
@@ -29,61 +29,69 @@ namespace DarPoolingNode
             return result.ToArray();
         }
 
+        // Start The Service Nodes
         static void Main(string[] args)
         {
-   /*         using (ServiceHost sh = new ServiceHost(typeof(DarPooling)))
+            Console.WriteLine("**** Starting the Backbone Nodes... ****");
+  
+            try
             {
-                sh.Open();
-                Console.WriteLine("Service Starts...");
-                Console.ReadLine();
-                sh.Close();
+
+                WSHttpBinding bind = new WSHttpBinding();
+                Type contract = typeof(IDarPooling);
+
+                ServiceHost milano = new ServiceHost(typeof(DarPooling), new Uri("http://localhost:1111/Milano"));
+                ServiceHost roma = new ServiceHost(typeof(DarPooling), new Uri("http://localhost:1111/Roma"));
+
+                roma.AddServiceEndpoint(contract, bind, "");
+                milano.AddServiceEndpoint(contract, bind, "");
+                
+
+                ServiceMetadataBehavior ro_behavior = new ServiceMetadataBehavior();
+                ro_behavior.HttpGetEnabled = true;
+
+                ServiceMetadataBehavior mi_behavior = new ServiceMetadataBehavior();
+                mi_behavior.HttpGetEnabled = true;
+                
+                roma.Description.Behaviors.Add(ro_behavior);
+                milano.Description.Behaviors.Add(mi_behavior);
+
+                roma.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+                milano.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+
+                roma.Open();
+                Console.WriteLine("ROMA node is now active. Waiting...");
+                milano.Open();
+                Console.WriteLine("MILANO node is now active. Waiting...");
+                Console.Read();
+
+                roma.Close();
+                //
+                milano.Close();
             }
-            Console.ReadLine();
-     */
-            ServiceHost roma = new ServiceHost(typeof(DarPooling));
-            ServiceHost milano = new ServiceHost(typeof(DarPooling));
-
-            Uri roma_addr = new Uri("http://localhost:3333/Roma");
-            WSHttpBinding bind = new WSHttpBinding();
-            Type contract = typeof(IDarPooling);
-
-            roma.AddServiceEndpoint(contract, bind, roma_addr);
-
-            BindingElement bindingElement = new HttpTransportBindingElement();
-            CustomBinding binding =
-               new CustomBinding(bindingElement);
-            // bindingElement = new BasicHttpBinding();
-            //CustomBinding binding = new CustomBinding(bindingElement);
-            //Uri tcpBaseAddress = new Uri("net.tcp://localhost:9000/");
-            //ServiceHost host = new ServiceHost(typeof(MyService), tcpBaseAddress);
-            ServiceMetadataBehavior metadataBehavior;
-            metadataBehavior = roma.Description.Behaviors.Find<ServiceMetadataBehavior>();
-            if (metadataBehavior == null)
+            catch (Exception e)
             {
-                metadataBehavior = new ServiceMetadataBehavior();
-                roma.Description.Behaviors.Add(metadataBehavior);
+                Console.WriteLine(e.Message);
+                Console.Read();
             }
-            //roma.AddServiceEndpoint(typeof(IMetadataExchange), bind, "MEX");
-            //host.Open();
 
 
-            roma.Open();
 
+            //roma.Open();
 
-            Uri milano_addr = new Uri("http://localhost:3332/Milano");
-            WSHttpBinding mil_bind = new WSHttpBinding();
+            
          
 
-            milano.AddServiceEndpoint(contract, mil_bind, milano_addr);
+            //milano.AddServiceEndpoint(contract, mil_bind, milano_addr);
 
-            milano.Open();
+            //milano.Open();
 
-            Console.WriteLine("Service Starts...");
+          //  Console.WriteLine("Service Starts...");
 
-            DisplayHostInfo(roma);
-            DisplayHostInfo(milano);
+            //DisplayHostInfo(roma);
+            //DisplayHostInfo(milano);
            
-            Console.ReadLine();
+        //    Console.ReadLine();
         
         }
 
@@ -97,3 +105,34 @@ namespace DarPoolingNode
         }
     }
 }
+
+/*         using (ServiceHost sh = new ServiceHost(typeof(DarPooling)))
+           {
+               sh.Open();
+               Console.WriteLine("Service Starts...");
+               Console.ReadLine();
+               sh.Close();
+           }
+           Console.ReadLine();
+    */
+
+
+//BindingElement bindingElement = new HttpTransportBindingElement();
+//CustomBinding binding =
+// new CustomBinding(bindingElement);
+// bindingElement = new BasicHttpBinding();
+//CustomBinding binding = new CustomBinding(bindingElement);
+//Uri tcpBaseAddress = new Uri("net.tcp://localhost:9000/");
+//ServiceHost host = new ServiceHost(typeof(MyService), tcpBaseAddress);
+//ServiceMetadataBehavior metadataBehavior = new ServiceMetadataBehavior();
+//metadataBehavior.HttpGetEnabled = true;
+//roma.Description.Behaviors.Add(metadataBehavior);
+//roma.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+/*metadataBehavior = roma.Description.Behaviors.Find<ServiceMetadataBehavior>();
+if (metadataBehavior == null)
+{
+    metadataBehavior = new ServiceMetadataBehavior();
+    roma.Description.Behaviors.Add(metadataBehavior);
+}*/
+//roma.AddServiceEndpoint(typeof(IMetadataExchange), bind, "MEX");
+//host.Open();
