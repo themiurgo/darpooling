d1bace93abbbf4e8f168365ed00d6f3c1295ff1b
danielemar86 <danielemar86@gmail.com>
2010-06-30 23:02:26 +0200

    NOW Added TestClient class

---
+++ ClientCore/TestClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using Communication;
+
+namespace ClientCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDarPooling client = ChannelFactory<IDarPooling>.CreateChannel(
+                                 new WSHttpBinding(), new EndpointAddress(
+                                 "http://localhost:1111/Roma/"
+                                 ));
+            Console.WriteLine(client.SayHello());
+            Console.ReadLine();
+
+
+        }
+    }
+}
