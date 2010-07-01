147186d035a59fc80420eb51c1d3b0821ea52973
danielemar86 <danielemar86@gmail.com>
2010-07-01 18:59:19 +0200

    Code cleaning

--- DarPoolingNode/DarPooling.cs
+++ DarPoolingNode/DarPooling.cs
@@ -9,8 +9,6 @@ using System.ServiceModel.Description;
 using Communication;
 using System.Runtime.Serialization;
 
-using System.Threading;
-using System.Diagnostics;
 using System.Collections;
 
 
@@ -22,18 +20,17 @@ namespace DarPoolingNode
         public string nodeName { get; private set; }
  
         public IPeer Channel;
-        public IPeer Host;
+//        public IPeer Host;
         private ServiceHost host;
 
-        private DuplexChannelFactory<IPeer> _factory;
-        private readonly AutoResetEvent _stopFlag = new AutoResetEvent(false);
+//        private DuplexChannelFactory<IPeer> _factory;
+//        private readonly AutoResetEvent _stopFlag = new AutoResetEvent(false);
  
         public PeerNode(string nodeName)
         {
             this.nodeName = nodeName;
         }
 
-        
         public void StartService()
         {
 
@@ -48,19 +45,11 @@ namespace DarPoolingNode
             behavior.HttpGetEnabled = true;
 
             host.Description.Behaviors.Add(behavior);
-            
-
             host.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
             
-
             host.Open();
             Console.WriteLine(nodeName + "\t\tnode is now active. Waiting...");
-           
-            //Console.Read();
-
-            //host.Close();
-        
-            
+                       
             
            /* var peer_binding = new NetPeerTcpBinding();
             peer_binding.Security.Mode = SecurityMode.None;
@@ -86,16 +75,18 @@ namespace DarPoolingNode
 
         public void StopService()
         {
-           // ((ICommunicationObject)Channel).Close();
-           // if (_factory != null)
-           //     _factory.Close();
             Console.WriteLine("Closing the service host...");
             host.Close();
-
-
         }
-
-        public void Run()
+        
+        
+        public void DisplayHostInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("**** Host Info ****");
+            Console.WriteLine("Uri: {0}", host.State);
+        }
+/*        public void Run()
         {
             Console.WriteLine("[ Starting Service ]");
             StartService();
@@ -108,13 +99,7 @@ namespace DarPoolingNode
 
             Console.WriteLine("[ Service Stopped ]");
         }
-
-/*        public void Stop()
-        {
-            _stopFlag.Set();
-        }
-  */     
-
+ */
     }
 
     public class DarPooling : IDarPooling, IPeer
@@ -153,13 +138,13 @@ namespace DarPoolingNode
 
         private static ArrayList peers = new ArrayList();
 
-        // Start The Service Nodes
+        
         static void Main(string[] args)
         {
             Console.WriteLine("**** Starting the Backbone Nodes... ****\n");
             StartBackboneNodes();
 
-            Console.WriteLine("\nAll Service nodes are now Active");
+            Console.WriteLine("\nAll Service nodes are now Active \nPress enter to quit...");
 
             Console.ReadLine();
             CloseBackboneNodes();
@@ -170,41 +155,21 @@ namespace DarPoolingNode
         public static void StartBackboneNodes()
         {
             string[] nodeNames = { "Chiasso", "Milano", "Roma", "Napoli", "Catania" };
-            
-            
-            
+        
             foreach (string name in nodeNames)
             {
                 peers.Add(new PeerNode(name.ToUpper()));
-            
             }
 
             foreach (PeerNode peer in peers)
             {
                 peer.StartService();
             }
-
-                //Console.WriteLine(c);
-            //var peer = new PeerNode(name);
-            //var peerThread = new Thread(peer.StartService) {IsBackground = true};
-            //peerThread.Start();
  
-            //wait for the server to start up.
-            //Thread.Sleep(1000);
-            //peer.StartService();
-           /* while (true)
-            {
-                Console.Write("Enter Something: ");
-                string tmp = Console.ReadLine();
- 
-                if (tmp == "") break;
- 
-   //             peer.Channel.Ping(peer.Id, tmp);
-            }
- /*
-            peer.Stop();
-            peerThread.Join();
-            */
+            //peer.Channel.Ping(peer.Id, tmp);
+            //peer.Stop();
+            //peerThread.Join();
+           
         }
 
 
@@ -214,116 +179,7 @@ namespace DarPoolingNode
             {
                 peer.StopService();
             }
-        
         }
-
-
-        static void OLD_StartBackboneNodes()
-        {
-
-            try
-            {
-
-                WSHttpBinding bind = new WSHttpBinding();
-                Type contract = typeof(IDarPooling);
-
-                ServiceHost milano = new ServiceHost(typeof(DarPooling), new Uri("http://localhost:1111/Milano"));
-                ServiceHost roma = new ServiceHost(typeof(DarPooling), new Uri("http://localhost:1111/Roma"));
-
-                roma.AddServiceEndpoint(contract, bind, "");
-                milano.AddServiceEndpoint(contract, bind, "");
-
-
-                ServiceMetadataBehavior ro_behavior = new ServiceMetadataBehavior();
-                ro_behavior.HttpGetEnabled = true;
-
-                ServiceMetadataBehavior mi_behavior = new ServiceMetadataBehavior();
-                mi_behavior.HttpGetEnabled = true;
-
-                roma.Description.Behaviors.Add(ro_behavior);
-                milano.Description.Behaviors.Add(mi_behavior);
-
-                roma.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
-                milano.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
-
-                roma.Open();
-                Console.WriteLine("ROMA node is now active. Waiting...");
-                milano.Open();
-                Console.WriteLine("MILANO node is now active. Waiting...");
-                Console.Read();
-
-                roma.Close();
-                //
-                milano.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.Read();
-            }
-
-
-        }
-
-
-        static void DisplayHostInfo(ServiceHost host)
-        {
-            Console.WriteLine();
-            Console.WriteLine("**** Host Info ****");
-
-            Console.WriteLine("Uri: {0}", host.State);
-
-        }
-
-
 
     } //End Class
 } //End Namespace
-
-/*         using (ServiceHost sh = new ServiceHost(typeof(DarPooling)))
-           {
-               sh.Open();
-               Console.WriteLine("Service Starts...");
-               Console.ReadLine();
-               sh.Close();
-           }
-           Console.ReadLine();
-    */
-
-
-
-//roma.Open();
-
-
-
-
-//milano.AddServiceEndpoint(contract, mil_bind, milano_addr);
-
-//milano.Open();
-
-//  Console.WriteLine("Service Starts...");
-
-//DisplayHostInfo(roma);
-//DisplayHostInfo(milano);
-
-//    Console.ReadLine();
-
-//BindingElement bindingElement = new HttpTransportBindingElement();
-//CustomBinding binding =
-// new CustomBinding(bindingElement);
-// bindingElement = new BasicHttpBinding();
-//CustomBinding binding = new CustomBinding(bindingElement);
-//Uri tcpBaseAddress = new Uri("net.tcp://localhost:9000/");
-//ServiceHost host = new ServiceHost(typeof(MyService), tcpBaseAddress);
-//ServiceMetadataBehavior metadataBehavior = new ServiceMetadataBehavior();
-//metadataBehavior.HttpGetEnabled = true;
-//roma.Description.Behaviors.Add(metadataBehavior);
-//roma.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
-/*metadataBehavior = roma.Description.Behaviors.Find<ServiceMetadataBehavior>();
-if (metadataBehavior == null)
-{
-    metadataBehavior = new ServiceMetadataBehavior();
-    roma.Description.Behaviors.Add(metadataBehavior);
-}*/
-//roma.AddServiceEndpoint(typeof(IMetadataExchange), bind, "MEX");
-//host.Open();
