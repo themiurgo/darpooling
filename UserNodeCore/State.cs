2eadeecfc6a3c293f79be22c1debab5c6a44e710
anto87 <anto87@localhost>
2010-07-22 17:52:35 +0200

    Adding LocationRange

--- UserNodeCore/State.cs
+++ UserNodeCore/State.cs
@@ -9,9 +9,8 @@ namespace Client
 {
     public interface IState
     {
-        bool Join(UserNodeCore context, string address);
         bool RegisterUser(UserNodeCore context, string username, string pw_hash);
-        bool LoginUser(UserNodeCore context, string username, string pw_hash);
+        bool Join(UserNodeCore context, string username, string pw_hash);
         bool Unjoin(UserNodeCore context);
         bool InsertTrip(UserNodeCore context, Communication.Trip trip);
         bool SearchTrip(UserNodeCore context);
@@ -19,31 +18,12 @@ namespace Client
 
     public class UnjointState : IState
     {
-        public bool Join(UserNodeCore context, string address)
-        {
-            address = "http://localhost:1111/Milano";
-            WSDualHttpBinding binding = new WSDualHttpBinding();
-            ChannelFactory<IDarPooling> channelFactory = new ChannelFactory<IDarPooling>(binding);
-            EndpointAddress endpointAddress = new EndpointAddress(address);
-            IDarPooling proxy = channelFactory.CreateChannel(endpointAddress);
-
-            Command comm = new JoinCommand(context.UserNode);
-            proxy.SendCommand(comm);
-            // Console.WriteLine(proxy.SayHello());
-            // Connect operations
-            // ... Here should create proxy ...
-
-            // Change state to connected
-            context.State = new JointState();
-            return true;
-        }
-
         public bool RegisterUser(UserNodeCore context, string username, string pw_hash)
         {
             return false;
         }
 
-        public bool LoginUser(UserNodeCore context, string username, string pw_hash)
+        public bool Join(UserNodeCore context, string username, string pw_hash)
         {
             return false;
         }
@@ -66,11 +46,6 @@ namespace Client
 
     public class JointState : IState
     {
-        public bool Join(UserNodeCore context, string address)
-        {
-            return false;
-        }
-
         public bool Unjoin(UserNodeCore context)
         {
             context.State = new UnjointState();
@@ -82,7 +57,7 @@ namespace Client
             return false;
         }
 
-        public bool LoginUser(UserNodeCore context, string username, string pw_hash)
+        public bool Join(UserNodeCore context, string username, string pw_hash)
         {
             Console.WriteLine("LOGIN");
             return false;
@@ -101,11 +76,6 @@ namespace Client
 
     public class LoggedState : IState
     {
-        public bool Join(UserNodeCore context, string address)
-        {
-            return false;
-        }
-
         public bool Unjoin(UserNodeCore context)
         {
             context.State = new UnjointState();
@@ -117,7 +87,7 @@ namespace Client
             return false;
         }
 
-        public bool LoginUser(UserNodeCore context, string username, string pw_hash)
+        public bool Join(UserNodeCore context, string username, string pw_hash)
         {
             return false;
         }
