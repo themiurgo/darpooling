4c28a227b9a0130da41372ecbd4f68288422efbc
anto87 <anto87@localhost>
2010-07-22 15:23:32 +0200

    Adding UserNodeMobileGUI project

---
+++ UserNodeMobileGUI/DarpoolingForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UserNodeMobileGUI
+{
+    public partial class DarpoolingForm : Form
+    {
+
+
+        public DarpoolingForm()
+        {
+            Client.UserNodeCore = new Client.UserNodeCore(
+            // InitializeComponent();
+        }
+    }
+}
