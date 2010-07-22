3797c0557d862fa26c00d39698429e0bdc6f7624
anto87 <anto87@localhost>
2010-07-22 15:23:32 +0200

    Adding UserNodeMobileGUI project

---
+++ UserNodeMobileGUI/DarpoolingForm.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace UserNodeMobileGUI
+{
+    partial class DarpoolingForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.Menu = this.mainMenu1;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Text = "DarpoolingForm";
+            this.AutoScroll = true;
+        }
+
+        #endregion
+    }
+}
---
+++ UserNodeMobileGUI/LoginForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UserNodeMobileGUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void resetMenuItem_Click(object sender, EventArgs e)
+        {
+            usernameTextBox.Text = "";
+            passwordTextBox.Text = "";
+            serviceNodeAddressTextBox.Text = "";
+        }
+
+        private void loginMenuItem_Click(object sender, EventArgs e)
+        {
+            // Login procedure;
+            Form searchForm = new SearchTripForm();
+            searchForm.Show();
+        }
+
+    }
+}
