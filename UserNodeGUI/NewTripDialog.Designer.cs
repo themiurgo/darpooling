1d1cfcab8e31bc14079bb7aaf685faf9c067f202
anto87 <anto87@localhost>
2010-09-23 10:56:08 +0200

    NewTrip now creates a Trip object on OK. Updated InsertTrip command.

--- UserNodeGUI/NewTripDialog.Designer.cs
+++ UserNodeGUI/NewTripDialog.Designer.cs
@@ -28,31 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.departureNameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.arrivalNameTextBox = new System.Windows.Forms.TextBox();
+            this.freeSeatsUpDown = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.smokingCheckBox = new System.Windows.Forms.CheckBox();
+            this.musicCheckBox = new System.Windows.Forms.CheckBox();
+            this.contributeBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.departureDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.departureTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.arrivalDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.arrivalTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.freeSeatsUpDown)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // departureNameTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(72, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(307, 20);
-            this.textBox1.TabIndex = 0;
+            this.departureNameTextBox.Location = new System.Drawing.Point(72, 6);
+            this.departureNameTextBox.Name = "departureNameTextBox";
+            this.departureNameTextBox.Size = new System.Drawing.Size(307, 20);
+            this.departureNameTextBox.TabIndex = 0;
             // 
             // label1
             // 
@@ -72,19 +72,19 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Arrival";
             // 
-            // textBox2
+            // arrivalNameTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(72, 58);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(307, 20);
-            this.textBox2.TabIndex = 3;
+            this.arrivalNameTextBox.Location = new System.Drawing.Point(72, 58);
+            this.arrivalNameTextBox.Name = "arrivalNameTextBox";
+            this.arrivalNameTextBox.Size = new System.Drawing.Size(307, 20);
+            this.arrivalNameTextBox.TabIndex = 3;
             // 
-            // numericUpDown1
+            // freeSeatsUpDown
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(72, 110);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
-            this.numericUpDown1.TabIndex = 4;
+            this.freeSeatsUpDown.Location = new System.Drawing.Point(72, 110);
+            this.freeSeatsUpDown.Name = "freeSeatsUpDown";
+            this.freeSeatsUpDown.Size = new System.Drawing.Size(43, 20);
+            this.freeSeatsUpDown.TabIndex = 4;
             // 
             // label3
             // 
@@ -95,34 +95,34 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Free seats";
             // 
-            // checkBox1
+            // smokingCheckBox
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(252, 135);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(67, 17);
-            this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "Smoking";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.smokingCheckBox.AutoSize = true;
+            this.smokingCheckBox.Location = new System.Drawing.Point(252, 135);
+            this.smokingCheckBox.Name = "smokingCheckBox";
+            this.smokingCheckBox.Size = new System.Drawing.Size(67, 17);
+            this.smokingCheckBox.TabIndex = 6;
+            this.smokingCheckBox.Text = "Smoking";
+            this.smokingCheckBox.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // musicCheckBox
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(325, 135);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(54, 17);
-            this.checkBox2.TabIndex = 7;
-            this.checkBox2.Text = "Music";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.musicCheckBox.AutoSize = true;
+            this.musicCheckBox.Location = new System.Drawing.Point(325, 135);
+            this.musicCheckBox.Name = "musicCheckBox";
+            this.musicCheckBox.Size = new System.Drawing.Size(54, 17);
+            this.musicCheckBox.TabIndex = 7;
+            this.musicCheckBox.Text = "Music";
+            this.musicCheckBox.UseVisualStyleBackColor = true;
             // 
-            // textBox3
+            // contributeBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(72, 136);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(43, 20);
-            this.textBox3.TabIndex = 8;
-            this.textBox3.Text = "0";
-            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.contributeBox.Location = new System.Drawing.Point(72, 136);
+            this.contributeBox.Name = "contributeBox";
+            this.contributeBox.Size = new System.Drawing.Size(43, 20);
+            this.contributeBox.TabIndex = 8;
+            this.contributeBox.Text = "0";
+            this.contributeBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // label4
             // 
@@ -133,84 +133,86 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Contribute";
             // 
-            // dateTimePicker1
+            // departureDatePicker
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(72, 32);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 10;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.departureDatePicker.Location = new System.Drawing.Point(72, 32);
+            this.departureDatePicker.Name = "departureDatePicker";
+            this.departureDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.departureDatePicker.TabIndex = 10;
+            this.departureDatePicker.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
-            // dateTimePicker2
+            // departureTimePicker
             // 
-            this.dateTimePicker2.CustomFormat = "HH:mm";
-            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(278, 32);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.ShowUpDown = true;
-            this.dateTimePicker2.Size = new System.Drawing.Size(55, 20);
-            this.dateTimePicker2.TabIndex = 11;
+            this.departureTimePicker.CustomFormat = "HH:mm";
+            this.departureTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.departureTimePicker.Location = new System.Drawing.Point(278, 32);
+            this.departureTimePicker.Name = "departureTimePicker";
+            this.departureTimePicker.ShowUpDown = true;
+            this.departureTimePicker.Size = new System.Drawing.Size(55, 20);
+            this.departureTimePicker.TabIndex = 11;
             // 
-            // dateTimePicker3
+            // arrivalDatePicker
             // 
-            this.dateTimePicker3.Location = new System.Drawing.Point(72, 84);
-            this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker3.TabIndex = 12;
+            this.arrivalDatePicker.Location = new System.Drawing.Point(72, 84);
+            this.arrivalDatePicker.Name = "arrivalDatePicker";
+            this.arrivalDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.arrivalDatePicker.TabIndex = 12;
             // 
-            // dateTimePicker4
+            // arrivalTimePicker
             // 
-            this.dateTimePicker4.CustomFormat = "HH:mm";
-            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker4.Location = new System.Drawing.Point(278, 84);
-            this.dateTimePicker4.Name = "dateTimePicker4";
-            this.dateTimePicker4.ShowUpDown = true;
-            this.dateTimePicker4.Size = new System.Drawing.Size(55, 20);
-            this.dateTimePicker4.TabIndex = 13;
+            this.arrivalTimePicker.CustomFormat = "HH:mm";
+            this.arrivalTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.arrivalTimePicker.Location = new System.Drawing.Point(278, 84);
+            this.arrivalTimePicker.Name = "arrivalTimePicker";
+            this.arrivalTimePicker.ShowUpDown = true;
+            this.arrivalTimePicker.Size = new System.Drawing.Size(55, 20);
+            this.arrivalTimePicker.TabIndex = 13;
             // 
-            // button1
+            // CancelButton
             // 
-            this.button1.Location = new System.Drawing.Point(304, 175);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Cancel";
-            this.button1.UseVisualStyleBackColor = true;
+            this.CancelButton.Location = new System.Drawing.Point(304, 175);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 14;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
             // 
-            // button2
+            // OKButton
             // 
-            this.button2.Location = new System.Drawing.Point(223, 175);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 15;
-            this.button2.Text = "OK";
-            this.button2.UseVisualStyleBackColor = true;
+            this.OKButton.Location = new System.Drawing.Point(223, 175);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 15;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
             // 
             // NewTripDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(393, 210);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dateTimePicker4);
-            this.Controls.Add(this.dateTimePicker3);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.arrivalTimePicker);
+            this.Controls.Add(this.arrivalDatePicker);
+            this.Controls.Add(this.departureTimePicker);
+            this.Controls.Add(this.departureDatePicker);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.contributeBox);
+            this.Controls.Add(this.musicCheckBox);
+            this.Controls.Add(this.smokingCheckBox);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.freeSeatsUpDown);
+            this.Controls.Add(this.arrivalNameTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.departureNameTextBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "NewTripDialog";
             this.Text = "New Trip";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.freeSeatsUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -218,21 +220,21 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox departureNameTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox arrivalNameTextBox;
+        private System.Windows.Forms.NumericUpDown freeSeatsUpDown;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox smokingCheckBox;
+        private System.Windows.Forms.CheckBox musicCheckBox;
+        private System.Windows.Forms.TextBox contributeBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker departureDatePicker;
+        private System.Windows.Forms.DateTimePicker departureTimePicker;
+        private System.Windows.Forms.DateTimePicker arrivalDatePicker;
+        private System.Windows.Forms.DateTimePicker arrivalTimePicker;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button OKButton;
     }
 }
