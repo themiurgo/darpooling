13632377b5a9910285ee39987e5cdff6eea8dfc2
danielemar86 <danielemar86@gmail.com>
2010-09-27 21:29:39 +0200

    Added support for query hash ID in querybuilder and SearchTripResult

---
+++ Communication/Result.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.Serialization;
+
+namespace Communication
+{
+    [DataContract]
+    [KnownType(typeof(NullResult))]
+    [KnownType(typeof(ConnectionErrorResult))]
+    [KnownType(typeof(RegisterOkResult))]
+    [KnownType(typeof(RegisterErrorResult))]
+    [KnownType(typeof(LoginOkResult))]
+    [KnownType(typeof(LoginErrorResult))]
+    [KnownType(typeof(UnjoinConfirmedResult))]
+    [KnownType(typeof(InsertErrorResult))]
+    [KnownType(typeof(InsertOkResult))]
+    [KnownType(typeof(SearchTripResult))]
+    [KnownType(typeof(SearchTripError))]
+    public abstract class Result
+    {
+        [DataMember]
+        protected string comment;
+
+        public string Comment
+        {
+            get { return comment; }
+            set { comment = value; }
+        }
+    }
+
+
+
+    // General Purpose Result
+    [DataContract]
+    public class NullResult : Result { }
+
+    [DataContract]
+    public class ConnectionErrorResult : Result { }
+
+
+    // Possible results in response to RegisterUserCommand
+
+    [DataContract]
+    public class RegisterOkResult : Result 
+    {
+        [DataMember]
+        private string registeredUsername;
+
+        public string FinalUsername
+        {
+            get { return registeredUsername; }
+            set { registeredUsername = value; }
+        }
+    }
+    [DataContract]
+    public class RegisterErrorResult : Result { }
+
+
+    // Possible results in response to JoinCommand
+    
+    [DataContract]
+    public class LoginOkResult : Result 
+    {
+        [DataMember]
+        string authorizedUsername;
+
+        public string AuthorizedUsername
+        {
+            get { return authorizedUsername; }
+            set { authorizedUsername = value; }
+        }
+    }
+    [DataContract]
+    public class LoginErrorResult : Result { }
+    
+
+    // Possible results in response to UnjoinCommand
+    
+    [DataContract]
+    public class UnjoinConfirmedResult : Result { }
+
+    
+    // Possible results in response to InsertTrip
+    
+    [DataContract]
+    public class InsertOkResult : Result { }
+    [DataContract]
+    public class InsertErrorResult : Result { }
+
+    
+    // Possible results in response to SearchTrip
+    
+    [DataContract]
+    public class SearchTripResult : Result
+    {
+        [DataMember]
+        private List<Trip> trips;
+        [DataMember]
+        private string originalQueryID;
+
+        public List<Trip> Trips
+        {
+            get { return trips; }
+        }
+
+        public SearchTripResult(List<Trip> trips)
+        {
+            this.trips = trips;
+        }
+
+        public string OriginalQueryID
+        {
+            get { return originalQueryID; }
+            set { originalQueryID = value; }
+        }
+
+    }
+    
+    [DataContract]
+    public class SearchTripError : Result { }
+
+
+    /// <summary>
+    /// This result is used when working with callbacks is not possible
+    /// (i.e. smartphone devices). It simply says to wait for a given (or
+    /// arbitrary) amount of time and then try again requesting the resource.
+    /// </summary>
+    public class WaitAndTryResult : Result
+    {
+        public int Seconds { get; set; }
+    }
+}
---
+++ UserNodeGUI/RegisterUserDialog.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace UserNodeGUI
+{
+    partial class RegisterUserDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.locationTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.femaleRadioButton = new System.Windows.Forms.RadioButton();
+            this.maleRadioButton = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.birthdateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.registrarLabel = new System.Windows.Forms.Label();
+            this.registrarTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.smokerCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.smokerCheckBox);
+            this.groupBox2.Controls.Add(this.locationTextBox);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.femaleRadioButton);
+            this.groupBox2.Controls.Add(this.maleRadioButton);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.birthdateTimePicker);
+            this.groupBox2.Controls.Add(this.emailTextBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.nameTextBox);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 116);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 171);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "User details";
+            // 
+            // locationTextBox
+            // 
+            this.locationTextBox.Location = new System.Drawing.Point(67, 122);
+            this.locationTextBox.Name = "locationTextBox";
+            this.locationTextBox.Size = new System.Drawing.Size(127, 20);
+            this.locationTextBox.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Location";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Sex";
+            // 
+            // femaleRadioButton
+            // 
+            this.femaleRadioButton.AutoSize = true;
+            this.femaleRadioButton.Location = new System.Drawing.Point(127, 99);
+            this.femaleRadioButton.Name = "femaleRadioButton";
+            this.femaleRadioButton.Size = new System.Drawing.Size(59, 17);
+            this.femaleRadioButton.TabIndex = 6;
+            this.femaleRadioButton.TabStop = true;
+            this.femaleRadioButton.Text = "Female";
+            this.femaleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // maleRadioButton
+            // 
+            this.maleRadioButton.AutoSize = true;
+            this.maleRadioButton.Location = new System.Drawing.Point(71, 99);
+            this.maleRadioButton.Name = "maleRadioButton";
+            this.maleRadioButton.Size = new System.Drawing.Size(48, 17);
+            this.maleRadioButton.TabIndex = 2;
+            this.maleRadioButton.TabStop = true;
+            this.maleRadioButton.Text = "Male";
+            this.maleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Birthdate";
+            // 
+            // birthdateTimePicker
+            // 
+            this.birthdateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.birthdateTimePicker.Location = new System.Drawing.Point(67, 73);
+            this.birthdateTimePicker.Name = "birthdateTimePicker";
+            this.birthdateTimePicker.Size = new System.Drawing.Size(100, 20);
+            this.birthdateTimePicker.TabIndex = 4;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(67, 46);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(127, 20);
+            this.emailTextBox.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Email";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(67, 19);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(127, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Name";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(56, 313);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(75, 23);
+            this.registerButton.TabIndex = 6;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(137, 313);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(67, 19);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(127, 20);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(67, 45);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(127, 20);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // registrarLabel
+            // 
+            this.registrarLabel.AutoSize = true;
+            this.registrarLabel.Location = new System.Drawing.Point(6, 74);
+            this.registrarLabel.Name = "registrarLabel";
+            this.registrarLabel.Size = new System.Drawing.Size(49, 13);
+            this.registrarLabel.TabIndex = 6;
+            this.registrarLabel.Text = "Registrar";
+            // 
+            // registrarTextBox
+            // 
+            this.registrarTextBox.Location = new System.Drawing.Point(67, 71);
+            this.registrarTextBox.Name = "registrarTextBox";
+            this.registrarTextBox.Size = new System.Drawing.Size(127, 20);
+            this.registrarTextBox.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.registrarTextBox);
+            this.groupBox1.Controls.Add(this.registrarLabel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.passwordTextBox);
+            this.groupBox1.Controls.Add(this.usernameTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 98);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Account informations";
+            // 
+            // smokerCheckBox
+            // 
+            this.smokerCheckBox.AutoSize = true;
+            this.smokerCheckBox.Location = new System.Drawing.Point(67, 148);
+            this.smokerCheckBox.Name = "smokerCheckBox";
+            this.smokerCheckBox.Size = new System.Drawing.Size(62, 17);
+            this.smokerCheckBox.TabIndex = 7;
+            this.smokerCheckBox.Text = "Smoker";
+            this.smokerCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // RegisterUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 348);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "RegisterUser";
+            this.Text = "RegisterUser";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton maleRadioButton;
+        private System.Windows.Forms.RadioButton femaleRadioButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker birthdateTimePicker;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox locationTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label registrarLabel;
+        private System.Windows.Forms.TextBox registrarTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox smokerCheckBox;
+    }
+}
