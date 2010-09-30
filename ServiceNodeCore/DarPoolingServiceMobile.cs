8703045b756ef5e5f40d65c53693c227e4bedd11
danielemar86 <danielemar86@gmail.com>
2010-09-30 11:43:01 +0200

    Added service mobile

---
+++ ServiceNodeCore/DarPoolingServiceMobile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Communication;
+using System.ServiceModel;
+
+namespace ServiceNodeCore
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    class DarPoolingServiceMobile : IDarPoolingMobile
+    {
+        Command c;
+
+        #region IDarPoolingMobile Members
+
+        string IDarPoolingMobile.HandleDarPoolingMobileRequest(Command c)
+        {
+            string timestamp = DateTime.Now.ToString();
+            c.CommandID = Tools.HashString(timestamp);
+            return timestamp;
+        }
+
+        Result IDarPoolingMobile.GetMobileResult(string requestID)
+        {
+            Result r = new NullResult();
+            r.Comment = "Your request is:" + requestID;
+            return r;
+        }
+
+        #endregion
+    }
+}
