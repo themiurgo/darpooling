214e8e62c80ac3e3d7aec82be474bbf8c767f945
danielemar86 <danielemar86@gmail.com>
2010-10-01 10:23:22 +0200

    Added a Test project for mobile. It is necessary to test the behaviour of the service.

---
+++ TestMobile/TestForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TestMobile
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.connectionLabel = new System.Windows.Forms.Label();
+            this.resultButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(40, 38);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(72, 20);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect";
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // connectionLabel
+            // 
+            this.connectionLabel.Location = new System.Drawing.Point(40, 73);
+            this.connectionLabel.Name = "connectionLabel";
+            this.connectionLabel.Size = new System.Drawing.Size(100, 20);
+            this.connectionLabel.Text = "Ready";
+            // 
+            // resultButton
+            // 
+            this.resultButton.Location = new System.Drawing.Point(40, 125);
+            this.resultButton.Name = "resultButton";
+            this.resultButton.Size = new System.Drawing.Size(72, 20);
+            this.resultButton.TabIndex = 2;
+            this.resultButton.Text = "Get Result";
+            this.resultButton.Click += new System.EventHandler(this.resultButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Location = new System.Drawing.Point(52, 162);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(131, 56);
+            this.resultLabel.Text = "None";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.resultButton);
+            this.Controls.Add(this.connectionLabel);
+            this.Controls.Add(this.connectButton);
+            this.Menu = this.mainMenu1;
+            this.Name = "TestForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label connectionLabel;
+        private System.Windows.Forms.Button resultButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
